5e4b14125f3c4a5e4d4183400405a5d326ca69c7
ashish936 <bishtashish936@gmail.com>
2020-04-21 19:07:59 +0530

    first commit

---
+++ src/component/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoItem(){
+    return(
+        <div className='todo-item'>
+             <input type="checkbox"/> <p>placeholder</p>
+        </div>
+    )
+}
+
+
+export default TodoItem
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './component/App.js'
+import'./App.css'
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
